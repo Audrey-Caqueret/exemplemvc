33a8a8b182200f1ecc502b45bb1209fddc403010
Audrey Caqueret <Audrey Caqueret@AUDREYCAQUERET>
2017-11-13 16:56:34 +0100

    Ajoutez des fichiers projet.

---
+++ ExempleAvecGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExempleAvecGit.Startup))]
+namespace ExempleAvecGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
